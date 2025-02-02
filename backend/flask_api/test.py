1fa6a9703a9d1d0c8f84c587f1c01e8f9747bf93
demonking-mw <180680416+demonking-mw@users.noreply.github.com>
2025-02-02 14:41:41 -0500

    api restructured for clarity

--- backend/flask_api/test.py
+++ backend/flask_api/test.py
@@ -4,7 +4,7 @@ BASE = "http://127.0.0.1:5000"
 
 response = requests.put(
     BASE + "/user",
-    json={"action": "mod_tier", "user_name": "bob", "tier": "pro"},
+    json={"action": "change", "user_name": "bob", "pwd": "bobsmith", "new_pwd": "smth2"},
     timeout=10,
 )
 print(response.json())
