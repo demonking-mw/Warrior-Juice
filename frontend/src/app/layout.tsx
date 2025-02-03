4e88db9f89f63224fbad46e249f95a2299d08db9
Owen Tan <tanowen04@gmail.com>
2025-02-03 18:44:22 -0500

    Added ability to click logo to move to homepage

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,19 +6,33 @@ import { type Metadata } from "next";
 import { TRPCReactProvider } from "~/trpc/react";
 import Link from "next/link";
 
+import logo from "../images/icon.png";
+
 function Header() {
   return (
     <header className ="bg-black py-1">
       <div className ="container mx-auto px-10">
         <nav className ="flex items-center justify-between text-yellow-500 font-semibold">
           <div className = "flex items-center">
-            <Link href="/">Home</Link>
+            <Link href="/">
+            <div>
+              <img 
+                src="/images/icon.jpg"
+                alt="logo image"
+                style={{ width: "40px", height: "30px" }} 
+              />
+            </div>
+            </Link>
           </div>
           <div>
-            <Link href="/testroute">Route</Link>
+            <Link href="/testroute">
+            Route
+            </Link>
           </div>
           <div>
-            <Link href="/testroute2">Route2</Link>
+            <Link href="/testroute2">
+            Route2
+            </Link>
           </div>
         </nav>
       </div>
