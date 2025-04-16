776cdb01007c306b8bf2bd9fd8eaa48b8a12a6fe
demonking-mw <m574wang@uwaterloo.ca>
2025-04-16 18:03:01 -0400

    basic fail tests added for user class

--- backend/api_tests/eup_login.py
+++ backend/api_tests/eup_login.py
@@ -13,5 +13,13 @@ if exit_code == 0:
     req_json = {"reauth_jwt": reauth_jwt, "uid": "bob", "act_ids": "1,3"}
     exit_code, response = RG.post("/user/info", req_json, debug_mode=True)
 
+## Failed login
+req_json = {"type": "eup", "action": "login", "uid": "bob", "pwd": "dinner_please"}
+new_code, response = RG.post("/user", req_json)
+if new_code == 0:
+    print("ERROR: login with wrong password should fail")
+    exit_code = -69  # pylint: disable=invalid-name
+else:
+    print("login with wrong password failed as expected")
 
 sys.exit(exit_code)
--- backend/api_tests/user_add_drop_test.py
+++ backend/api_tests/user_add_drop_test.py
@@ -15,27 +15,51 @@ response = requests.post(
     },
     timeout=35,
 )
+EXIT_CODE = 0
 if response.json().get("status"):
     print("created\n")
-    del_response = requests.put(
-        BASE + "/user",
-        json={
-            "auth_type": "eup",
-            "action": "delete",
-            "uid": "juice",
-            "user_name": "jocelyn",
-            "pwd": "0123",
-        },
-        timeout=35,
-    )
-    if del_response.json().get("status"):
-        print("deleted")
-        sys.exit(0)
-    else:
-        print("delete failed")
-        print(del_response.json())
-        sys.exit(3)
+
 else:
     print("create failed")
     print(response.json())
-    sys.exit(3)
+    EXIT_CODE = 3
+
+# Duplicate signup
+response = requests.post(
+    BASE + "/user",
+    json={
+        "type": "eup",
+        "action": "signup",
+        "uid": "juice",
+        "user_name": "jocelyn",
+        "pwd": "0123",
+        "email": "juice@workaholic.lol",
+    },
+    timeout=35,
+)
+if response.json().get("status"):
+    print("Duplicate create should fail")
+    EXIT_CODE = -69
+
+else:
+    print("create failed as expected")
+
+del_response = requests.put(
+    BASE + "/user",
+    json={
+        "auth_type": "eup",
+        "action": "delete",
+        "uid": "juice",
+        "user_name": "jocelyn",
+        "pwd": "0123",
+    },
+    timeout=35,
+)
+if del_response.json().get("status"):
+    print("deleted")
+else:
+    print("delete failed")
+    print(del_response.json())
+    EXIT_CODE = 3
+
+sys.exit(EXIT_CODE)
